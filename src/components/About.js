488bcf3b108f1516039cf557db1ea4418d57792d
parth22 <parthb7200@gmail.com>
2023-07-14 20:48:28 +0530

    added

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,20 @@
 import React from 'react'
-import bgimg from '../images/abtimg.jpg'
+import bgimg from '../images/about.jpg'
 function About() {
   return (
-    <div className=''>
+    <div className="about-div">
+      <h1>About Us</h1>
       <div class="row">
-            <div class="about">
+            <div className="about">
                 <h1>We are Largest IT Institute In Satara</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint voluptatem eaque accusantium blanditiis illo dolorem unde nihil dolore numquam nesciunt, quod consectetur corporis? Ad qui voluptas, ipsum voluptate consectetur odio!</p>
                 <a href="" class="text-btn">EXPLORE NOW</a>
             </div>
+            <div className='abt-img'>
+              <img src={bgimg} alt="" srcset="" />
+            </div>
         </div>
-    </div>
+        </div>
   )
 }
 
